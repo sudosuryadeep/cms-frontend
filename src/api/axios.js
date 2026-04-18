22214e0c93cc684668b51c6218e154fb4d9e5313
Suryadeep Singh <singhs01785@gmail.com>
2026-04-18 15:26:10 +0530

    main

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api/v1",
+  baseURL: "/api/v1",
   headers: {
     "Content-Type": "application/json",
+    "ngrok-skip-browser-warning": "true", 
   },
 });
 
--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://caseous-kristina-unrejectable.ngrok-free.dev",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 })
